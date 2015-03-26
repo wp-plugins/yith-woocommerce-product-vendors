c2c75cb3e1eabaf783104428ff58ab91b9202ea0
yithemes <yithemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 10:39:01 +0000

    version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/yith-woocommerce-product-vendors@1121241 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.1/templates/woocommerce/single-product/vendor-tab.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file belongs to the YIT Framework.
+ *
+ * This source file is subject to the GNU GENERAL PUBLIC LICENSE (GPL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+
+<h2>
+    <a href="<?php echo $vendor_url ?>">
+        <?php echo $vendor_name ?>
+    </a>
+</h2>
+
+<div class="vendor-description">
+    <?php echo $vendor_description ?>
+</div>
